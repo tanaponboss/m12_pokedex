b8d69efb46f442a199c149a9cbcd0bc1b007e737
tanaponboss <bossep9@gmail.com>
2023-11-15 21:23:53 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+      <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+  
+      <div class="wrapper">
+        <!-- <HelloWorld msg="You did it!" /> -->
+  
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/book/100001">100001</RouterLink>
+        </nav>
+      </div>
+    </header>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+#layout {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  -webkit-font-smooting: antialiased;
+  -moz-osx-font-smooting: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+defineProps({
+  book: {
+    type: Object,
+    required: true,
+  }
+})
+
+</script>
+
+<template>
+  <!-- <RouterLink to="book/100001"> -->
+  <RouterLink :to="{ name: 'book-details', params: {id: book.id} }" >
+    <div class="product-card">
+      <h2>{{ book.title }}</h2>
+      <span>Price: {{ book.price }} B , Condition: {{ book.condition }}</span>
+    </div>
+  </RouterLink>
+</template>
+
+<style scoped>
+.product-card {
+  padding: 20px;
+  width: 250px;
+  Height: 180px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+  float: left;
+  margin-inline: 10px;
+  display: flexbox;
+}
+.product-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
